56d0bf3876d7c8824e54b691fae1723832003400
GesS016 <gusikasadov@gmail.com>
2023-03-09 19:12:27 +0400

    DriverStorage created

---
+++ MoviesSocialNetwork/MoviesSocialNetwork/Drivers/DriverStorage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesSocialNetwork.Drivers
+{
+    public class DriverStorage
+    {
+        public WebDriver Driver { get; private set; }
+        
+        public static DriverStorage _instance;
+
+        private DriverStorage()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+        }
+
+        public static DriverStorage GetInstance()
+        {
+            if(_instance==null)
+            {
+                _instance= new DriverStorage();
+            }
+            return _instance;          
+        }
+    }
+}
