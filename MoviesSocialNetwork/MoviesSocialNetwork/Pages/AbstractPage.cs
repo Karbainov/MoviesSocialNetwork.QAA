7fa0a6f1d798e7fd6aed3592f4551965c2d39b7f
nailyasarkarova <nailyasarkarova001@gmail.com>
2023-03-11 14:38:03 +0400

    abstracat page

---
+++ MoviesSocialNetwork/MoviesSocialNetwork/Pages/AbstractPage.cs
@@ -0,0 +1,26 @@
+﻿using MoviesSocialNetwork.Drivers;
+using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+[assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)]
+
+namespace MoviesSocialNetwork.Pages
+{
+    public abstract class AbstractPage
+    {
+        protected WebDriver _driver;
+
+        public abstract void Open();
+
+        public AbstractPage()
+        {
+            _driver = DriverStorage.GetInstance().Driver;
+        }
+        public virtual void PressButton(IWebElement button)
+        {
+            Actions actions = new Actions(_driver);
+            actions.DoubleClick(button).Perform();
+        }
+    }
+}
